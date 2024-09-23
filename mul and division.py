8f9070a7df7631f12cefb0c7307c461346f6645f
nikil basil <nikilshibu01@gmail.com>
2024-09-23 02:52:42 -0700

    mul and division

---
+++ mul and division.py
@@ -0,0 +1,11 @@
+def multiplcation(x):
+    return x*x
+
+def division(x,y):
+    return x/y
+
+multi = int(input("Enter a number: "))
+division_x = int(input("Enter a number: "))
+division_y = int(input("Enter a number: "))
+print(multiplcation(multi))
+print(division(x=division_x,y=division_y))
